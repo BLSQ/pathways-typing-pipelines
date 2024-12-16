df4142e90aca4208260492437a37ee860c1e915b
Yann Forget <yannforget@mailbox.org>
2024-12-16 09:45:49 +0100

    feat(create-cart-diagram): upgrade to latest pathways-typing

--- create-cart-diagram/pipeline.py
+++ create-cart-diagram/pipeline.py
@@ -1,11 +1,12 @@
+from __future__ import annotations
+
 import json
 from datetime import datetime
 from pathlib import Path
-from typing import Optional, Tuple
 
 from openhexa.sdk import Dataset, current_run, parameter, pipeline, workspace
-from pathways.typing.mermaid import cart_diagram
-from pathways.typing.tree import build_binary_tree, merge_trees
+from pathways.typing.mermaid import create_cart_diagram
+from pathways.typing.tree import build_tree, merge_trees, parse_rpart
 
 
 @pipeline("create-cart-diagram", name="Create CART diagram")
@@ -19,7 +20,7 @@ from pathways.typing.tree import build_binary_tree, merge_trees
 @parameter(
     "version_name",
     name="Dataset version",
-    help="You can optionally specify the dataset version to use. If not specified, the latest version will be used.",
+    help="You can specify the dataset version to use. If not specified, latest version is used.",
     type=str,
     required=False,
 )
@@ -43,37 +44,27 @@ from pathways.typing.tree import build_binary_tree, merge_trees
     "output_dir",
     name="Output directory",
     help="If not specified, outputs will be saved into `workspace/typing/data/output/cart_diagram`",
+    default="typing/data/cart-diagram",
     type=str,
     required=False,
 )
-def create_cart_diagram(
+def generate_cart_diagram(
     cart_outputs: Dataset,
-    version_name: Optional[str],
-    add_choice_labels: Optional[bool],
-    add_node_id: Optional[bool],
-    output_dir: Optional[str],
-):
+    version_name: str,
+    output_dir: str,
+) -> None:
     """Create a CART diagram from CART outputs."""
+    urban, rural, version = load_dataset(dataset=cart_outputs, version_name=version_name)
 
-    urban, rural, version = load_dataset(
-        dataset=cart_outputs, version_name=version_name
+    output_dir = Path(
+        workspace.files_path,
+        output_dir,
+        version,
+        datetime.now().astimezone().strftime("%Y-%m-%d_%H:%M:%S"),
     )
-
-    if output_dir:
-        output_dir = Path(workspace.files_path, output_dir)
-    else:
-        output_dir = Path(
-            workspace.files_path,
-            "typing",
-            "data",
-            "output",
-            "cart_diagram",
-            version,
-            datetime.now().strftime("%Y-%m-%d_%H:%M:%S"),
-        )
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    generate_diagram(
+    generate_mermaid(
         urban_cart=urban,
         rural_cart=rural,
         output_dir=output_dir,
@@ -83,7 +74,7 @@ def create_cart_diagram(
 
 def load_dataset(
     dataset: Dataset, version_name: str | None = None
-) -> Tuple[list[dict], list[dict], str]:
+) -> tuple[list[dict], list[dict], str]:
     """Load urban and rural JSON files from dataset.
 
     Parameters
@@ -124,9 +115,9 @@ def load_dataset(
     urban: list[dict] = None
     rural: list[dict] = None
     for f in ds.files:
-        if f.filename.endswith("urban.json"):
+        if f.filename.endswith("cart_urban.json"):
             urban = json.loads(f.read().decode())
-        if f.filename.endswith("rural.json"):
+        if f.filename.endswith("cart_rural.json"):
             rural = json.loads(f.read().decode())
 
     if urban is None:
@@ -141,9 +132,9 @@ def load_dataset(
     return urban, rural, ds.name
 
 
-def generate_diagram(
+def generate_mermaid(
     urban_cart: list[dict], rural_cart: list[dict], output_dir: Path, version_name: str
-):
+) -> None:
     """Generate a mermaid diagram from urban and rural CART outputs.
 
     Both trees are merged into a single tree before generating the diagram.
@@ -159,36 +150,30 @@ def generate_diagram(
     version_name : str
         The name of the dataset version
     """
-    try:
-        urban = build_binary_tree(urban_cart, strata="urban")
-        current_run.log_info("Successfully built urban tree")
-    except Exception as e:
-        current_run.log_error("Could not build urban tree")
-        raise e
+    rural = parse_rpart(
+        nodes=rural_cart["nodes"],
+        ylevels=rural_cart["ylevels"],
+        xlevels=rural_cart["xlevels"],
+        csplit=rural_cart["csplit"],
+    )
+    current_run.log_info("Successfully parsed rural CART")
 
-    try:
-        rural = build_binary_tree(rural_cart, strata="rural")
-        current_run.log_info("Successfully built rural tree")
-    except Exception as e:
-        current_run.log_error("Could not build rural tree")
-        raise e
+    urban = parse_rpart(
+        nodes=urban_cart["nodes"],
+        ylevels=urban_cart["ylevels"],
+        xlevels=urban_cart["xlevels"],
+        csplit=urban_cart["csplit"],
+    )
+    current_run.log_info("Successfully parsed urban CART")
 
-    try:
-        root = merge_trees(urban, rural)
-        current_run.log_info("Successfully merged urban and rural trees")
-    except Exception as e:
-        current_run.log_error("Could not merge urban and rural trees")
-        raise e
+    root_rural = build_tree(rural, strata="rural")
+    root_urban = build_tree(urban, strata="urban")
+    root = merge_trees(root_rural, root_urban)
+    current_run.log_info("Successfully rebuilt CART tree")
 
-    try:
-        mermaid = cart_diagram(root)
-        current_run.log_info("Successfully generated mermaid diagram")
-    except Exception as e:
-        current_run.log_error("Could not generate mermaid diagram")
-        raise e
-
-    fp = output_dir / f"{version_name}_diagram.txt"
-    with open(fp, "w") as f:
+    mermaid = create_cart_diagram(root=root)
+    fp = output_dir / f"{version_name}.txt"
+    with fp.open("w") as f:
         f.write(mermaid)
 
-    current_run.add_file_output(fp.absolute().as_posix())
+    current_run.add_file_output(fp.as_posix())
